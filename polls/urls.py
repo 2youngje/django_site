0d80ecd30e314dea3c821be5b77aaac62e8e9383
lee youngje <dldudwp333@gmail.com>
2022-04-26 18:39:39 +0900

    Second commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,17 @@
+from unicodedata import name
+from unittest import result
 from urllib.parse import urlparse
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    #ex /polls/
+    path('',views.index,name='index'),
+    #ex /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    #ex /polls/5/reuslt/
+    path('<int:question_id>/result', views.results, name='results'),
+    #ex /polls/5/vote
+    path('<int:question_id>/vote/',views.vote, name='vote'),
 ]
